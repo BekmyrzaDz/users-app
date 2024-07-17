55b9ff2f924208e7a341ab1732ca1622f61dc6b5
Bekmyrza <dzhumakadyrov.b@gmail.com>
2024-07-17 17:29:55 +0600

    Clear unused code fragment

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { AuthButtonProps } from './Button.props'
-import styles from './Button.module.scss'
 import { clsx } from 'clsx'
+import styles from './Button.module.scss'
+import { AuthButtonProps } from './Button.props'
 
 const AuthButton = ({ children, className, ...props }: AuthButtonProps) => {
-  return (
-    <button {...props} className={clsx(styles.button, className)}>
-      {children}
-    </button>
-  )
+	return (
+		<button {...props} className={clsx(styles.button, className)}>
+			{children}
+		</button>
+	)
 }
 
 export default AuthButton
